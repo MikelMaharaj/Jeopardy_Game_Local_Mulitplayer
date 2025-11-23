a50ba48318b49b6780f78dd5c866b33793a49d66
Mikel Maharaj <mikelmaharaj25@gmail.com>
2025-11-22 21:58:37 -0400

    Added apidocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jeopardy.core"},{"l":"com.jeopardy.io"},{"l":"com.jeopardy.logging"},{"l":"com.jeopardy.report"},{"l":"com.jeopardy.ui"}];updateSearchResults();
